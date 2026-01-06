5a578c400ef80d25773172863609a692b9c4c2bf
peerapatpinyo-lgtm <peerapat.pinyo@mail.kmutt.ac.th>
2026-01-06 10:01:20 +0700

    Update solver.py

--- solver.py
+++ solver.py
@@ -7,9 +7,8 @@ class BeamSolver:
         self.spans = [float(s) for s in spans]
         self.E = float(E)
         self.b, self.h = b, h
-        # 1. จัดการค่า I
         self.I = float(I_custom) if I_custom else (b * h**3) / 12
-        # Timoshenko Constants: G = E / (2 * (1 + nu)), nu = 0.2, As = Shear Area Factor (5/6 for Rect)
+        # Timoshenko constants
         self.G = self.E / (2 * (1 + 0.2)) 
         self.As = (5/6) * (b * h)        
         self.cum_spans = [round(x, 4) for x in ([0.0] + list(np.cumsum(self.spans)))]
@@ -18,7 +17,6 @@ class BeamSolver:
 
     def _get_k_timoshenko(self, L):
         EI = self.E * self.I
-        # Phi (Shear Deformation Parameter)
         Phi = (12 * EI) / (L**2 * self.G * self.As)
         coeff = EI / (L**3 * (1 + Phi))
         return coeff * np.array([
@@ -30,7 +28,6 @@ class BeamSolver:
 
     def solve(self):
         try:
-            # --- Node Generation ---
             pts = self.cum_spans.copy()
             for _, l in self.loads_df.iterrows():
                 gx = self.cum_spans[int(l['span_index'])] + float(l['x'])
@@ -41,17 +38,14 @@ class BeamSolver:
             dof = 2 * num_nodes
             K, F = np.zeros((dof, dof)), np.zeros(dof)
 
-            # Equation Check Accumulators
             total_load_fy, total_load_m0 = 0.0, 0.0
 
-            # Stiffness Matrix Assembly
             for i in range(num_nodes - 1):
                 L = nodes[i+1] - nodes[i]
                 if L > 1e-5:
                     idx = [2*i, 2*i+1, 2*(i+1), 2*(i+1)+1]
                     K[np.ix_(idx, idx)] += self._get_k_timoshenko(L)
 
-            # --- 2. Load Application & Equation Check Logic ---
             for _, l in self.loads_df.iterrows():
                 gx = self.cum_spans[int(l['span_index'])] + float(l['x'])
                 mag = float(l['mag'])
@@ -66,17 +60,15 @@ class BeamSolver:
                     total_load_m0 -= mag 
                 elif l['type'] == 'U':
                     dist = float(l['dist'])
-                    s_g, e_g = gx, round(gx + dist, 4)
                     total_load_fy += mag * dist
                     total_load_m0 += (mag * dist) * (gx + dist/2)
                     for i in range(num_nodes - 1):
-                        overlap = min(e_g, nodes[i+1]) - max(s_g, nodes[i])
+                        overlap = min(gx+dist, nodes[i+1]) - max(gx, nodes[i])
                         if overlap > 1e-5:
                             w, L_el = mag, nodes[i+1] - nodes[i]
                             F[2*i] -= (w * L_el / 2); F[2*i+1] -= (w * L_el**2 / 12)
                             F[2*(i+1)] -= (w * L_el / 2); F[2*(i+1)+1] += (w * L_el**2 / 12)
 
-            # Boundary Conditions
             free_dof = np.full(dof, True)
             for _, sup in self.supports_df.iterrows():
                 if sup['type'] == "None": continue
@@ -88,19 +80,16 @@ class BeamSolver:
             U[free_dof] = solve(K[np.ix_(free_dof, free_dof)], F[free_dof])
             R_full = K @ U - F
 
-            # --- 3. Reaction & Statics Verification ---
             reac_list = []
             total_reac_fy, total_reac_m0 = 0.0, 0.0
             for _, sup in self.supports_df.iterrows():
                 if sup['type'] == "None": continue
-                node_idx = int(sup['id'])
-                nid = np.argmin([abs(n - self.cum_spans[node_idx]) for n in nodes])
-                ry, rm = R_full[2*nid], R_full[2*nid+1]
-                total_reac_fy += ry
-                total_reac_m0 += (ry * self.cum_spans[node_idx]) + rm
-                reac_list.append({'Node': node_idx, 'Type': sup['type'], 'Ry (kN)': round(ry/1000, 2), 'M (kNm)': round(rm/1000, 2)})
+                idx_n = int(sup['id'])
+                nid = np.argmin([abs(n - self.cum_spans[idx_n]) for n in nodes])
+                total_reac_fy += R_full[2*nid]
+                total_reac_m0 += (R_full[2*nid] * self.cum_spans[idx_n]) + R_full[2*nid+1]
+                reac_list.append({'Node': idx_n, 'Ry (kN)': round(R_full[2*nid]/1000, 2), 'M (kNm)': round(R_full[2*nid+1]/1000, 2)})
 
-            # Results Sampling
             res_data = []
             for x in np.linspace(0, nodes[-1], 400):
                 V, M, defl = 0.0, 0.0, 0.0
@@ -121,10 +110,5 @@ class BeamSolver:
                         defl = np.dot(H, U[2*i:2*i+4])
                         break
                 res_data.append({'x': x, 'shear': V, 'moment': M, 'deflection': defl})
-
-            return pd.DataFrame(res_data), pd.DataFrame(reac_list), {
-                'load_fy': total_load_fy, 'reac_fy': total_reac_fy,
-                'load_m0': total_load_m0, 'reac_m0': total_reac_m0
-            }
-        except Exception as e:
-            return pd.DataFrame(), pd.DataFrame(), {"error": str(e)}
+            return pd.DataFrame(res_data), pd.DataFrame(reac_list), {'load_fy': total_load_fy, 'reac_fy': total_reac_fy, 'load_m0': total_load_m0, 'reac_m0': total_reac_m0}
+        except Exception as e: return pd.DataFrame(), pd.DataFrame(), {"error": str(e)}
