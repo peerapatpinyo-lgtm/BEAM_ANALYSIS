b39573e737932ede47b5105060c3012e754719f9
peerapatpinyo-lgtm <peerapat.pinyo@mail.kmutt.ac.th>
2026-01-06 10:14:36 +0700

    Update solver.py

--- solver.py
+++ solver.py
@@ -8,8 +8,9 @@ class BeamSolver:
         self.E = float(E)
         self.b, self.h = b, h
         self.I = float(I_custom) if I_custom else (b * h**3) / 12
-        self.G = self.E / (2 * (1 + 0.2)) # Concrete Nu = 0.2
-        self.As = (5/6) * (b * h)        # Shear Area for Rectangular
+        # Timoshenko constants: G (Shear Modulus), As (Shear Area)
+        self.G = self.E / (2 * (1 + 0.2)) 
+        self.As = (5/6) * (b * h)        
         self.cum_spans = [round(x, 4) for x in ([0.0] + list(np.cumsum(self.spans)))]
         self.loads_df = pd.DataFrame(loads_input)
         self.supports_df = pd.DataFrame(supports_input)
@@ -31,6 +32,7 @@ class BeamSolver:
 
     def solve(self):
         try:
+            # Node identification
             pts = self.cum_spans.copy()
             for _, l in self.loads_df.iterrows():
                 gx = self.cum_spans[int(l['span_index'])] + float(l['x'])
@@ -41,83 +43,79 @@ class BeamSolver:
             dof = 2 * num_nodes
             K, F = np.zeros((dof, dof)), np.zeros(dof)
 
-            total_load_fy, total_load_m0 = 0.0, 0.0
+            t_load_fy, t_load_m0 = 0.0, 0.0
 
-            # Stiffness Assembly
+            # Assembly
             for i in range(num_nodes - 1):
                 L = nodes[i+1] - nodes[i]
                 if L > 1e-5:
-                    K[np.ix_([2*i, 2*i+1, 2*(i+1), 2*(i+1)+1], [2*i, 2*i+1, 2*(i+1), 2*(i+1)+1])] += self._get_k_timoshenko(L)
+                    idx = [2*i, 2*i+1, 2*(i+1), 2*(i+1)+1]
+                    K[np.ix_(idx, idx)] += self._get_k_timoshenko(L)
 
-            # Load Application
+            # Forces & Statics Check
             for _, l in self.loads_df.iterrows():
                 gx = self.cum_spans[int(l['span_index'])] + float(l['x'])
                 mag = float(l['mag'])
                 if l['type'] == 'P':
                     nid = np.argmin([abs(n - gx) for n in nodes]); F[2*nid] -= mag
-                    total_load_fy += mag; total_load_m0 += mag * gx
+                    t_load_fy += mag; t_load_m0 += mag * gx
                 elif l['type'] == 'M':
                     nid = np.argmin([abs(n - gx) for n in nodes]); F[2*nid+1] += mag
-                    total_load_m0 -= mag 
+                    t_load_m0 -= mag 
                 elif l['type'] == 'U':
                     dist = float(l['dist'])
-                    total_load_fy += mag * dist; total_load_m0 += (mag * dist) * (gx + dist/2)
+                    t_load_fy += mag * dist; t_load_m0 += (mag * dist) * (gx + dist/2)
                     for i in range(num_nodes - 1):
                         overlap = min(gx+dist, nodes[i+1]) - max(gx, nodes[i])
                         if overlap > 1e-5:
-                            w, L_el = mag, nodes[i+1] - nodes[i]
-                            F[2*i] -= (w * L_el / 2); F[2*i+1] -= (w * L_el**2 / 12)
-                            F[2*(i+1)] -= (w * L_el / 2); F[2*(i+1)+1] += (w * L_el**2 / 12)
+                            w, Le = mag, nodes[i+1] - nodes[i]
+                            F[2*i] -= (w * Le / 2); F[2*i+1] -= (w * Le**2 / 12)
+                            F[2*(i+1)] -= (w * Le / 2); F[2*(i+1)+1] += (w * Le**2 / 12)
 
-            free_dof = np.full(dof, True)
-            for _, sup in self.supports_df.iterrows():
-                if sup['type'] == "None": continue
-                nid = np.argmin([abs(n - self.cum_spans[int(sup['id'])]) for n in nodes])
-                if sup['type'] in ['Pin', 'Roller', 'Fixed']: free_dof[2*nid] = False
-                if sup['type'] == 'Fixed': free_dof[2*nid+1] = False
+            # Boundary Conditions
+            free_d = np.full(dof, True)
+            for _, s in self.supports_df.iterrows():
+                if s['type'] == "None": continue
+                nid = np.argmin([abs(n - self.cum_spans[int(s['id'])]) for n in nodes])
+                if s['type'] in ['Pin', 'Roller', 'Fixed']: free_d[2*nid] = False
+                if s['type'] == 'Fixed': free_d[2*nid+1] = False
 
-            U = solve(K[np.ix_(free_dof, free_dof)], F[free_dof])
-            U_full = np.zeros(dof)
-            U_full[free_dof] = U
-            R_full = K @ U_full - F
+            U = np.zeros(dof)
+            U[free_d] = solve(K[np.ix_(free_d, free_d)], F[free_d])
+            R = K @ U - F
 
-            # Reactions
-            reac_list = []
-            total_reac_fy, total_reac_m0 = 0.0, 0.0
-            for _, sup in self.supports_df.iterrows():
-                if sup['type'] == "None": continue
-                nid = np.argmin([abs(n - self.cum_spans[int(sup['id'])]) for n in nodes])
-                ry, rm = R_full[2*nid], R_full[2*nid+1]
-                total_reac_fy += ry
-                total_reac_m0 += (ry * self.cum_spans[int(sup['id'])]) + rm
-                reac_list.append({'Node': int(sup['id']), 'Type': sup['type'], 'Ry (kN)': round(ry/1000, 2), 'M (kNm)': round(rm/1000, 2)})
+            reac_res = []
+            t_reac_fy, t_reac_m0 = 0.0, 0.0
+            for _, s in self.supports_df.iterrows():
+                if s['type'] == "None": continue
+                nid = np.argmin([abs(n - self.cum_spans[int(s['id'])]) for n in nodes])
+                t_reac_fy += R[2*nid]; t_reac_m0 += (R[2*nid]*self.cum_spans[int(s['id'])]) + R[2*nid+1]
+                reac_res.append({'Node': int(s['id']), 'Type': s['type'], 'Ry (kN)': round(R[2*nid]/1000, 2), 'M (kNm)': round(R[2*nid+1]/1000, 2)})
 
-            # Plot Data
-            res_data = []
-            for x in np.linspace(0, nodes[-1], 500):
-                V, M, defl = 0.0, 0.0, 0.0
-                for i, n_p in enumerate(nodes):
-                    if n_p <= x + 1e-5:
-                        V += R_full[2*i]; M += R_full[2*i]*(x - n_p) + R_full[2*i+1]
+            # Results interpolation
+            res = []
+            for x in np.linspace(0, nodes[-1], 400):
+                v_sh, m_bm, d_defl = 0.0, 0.0, 0.0
+                for i, np_x in enumerate(nodes):
+                    if np_x <= x + 1e-5:
+                        v_sh += R[2*i]; m_bm += R[2*i]*(x - np_x) + R[2*i+1]
                 for _, l in self.loads_df.iterrows():
                     gx = self.cum_spans[int(l['span_index'])] + float(l['x'])
-                    if l['type'] == 'P' and gx <= x + 1e-5: V -= l['mag']; M -= l['mag']*(x - gx)
-                    elif l['type'] == 'M' and gx <= x + 1e-5: M -= l['mag']
+                    if l['type'] == 'P' and gx <= x + 1e-5: v_sh -= l['mag']; m_bm -= l['mag']*(x - gx)
+                    elif l['type'] == 'M' and gx <= x + 1e-5: m_bm -= l['mag']
                     elif l['type'] == 'U' and gx < x:
                         d = min(x, gx + l['dist']) - gx
-                        V -= l['mag']*d; M -= l['mag']*d*(x - (gx + d/2))
-                
+                        v_sh -= l['mag']*d; m_bm -= l['mag']*d*(x - (gx + d/2))
                 for i in range(num_nodes - 1):
                     if nodes[i] <= x <= nodes[i+1] + 1e-5:
-                        L_e, xi = nodes[i+1] - nodes[i], (x - nodes[i]) / (nodes[i+1] - nodes[i])
-                        Phi = self._get_phi(L_e)
-                        N1 = (1 / (1 + Phi)) * (1 - 3*xi**2 + 2*xi**3 + Phi*(1 - xi))
-                        N2 = (L_e / (1 + Phi)) * (xi - 2*xi**2 + xi**3 + 0.5*Phi*(xi - xi**2))
-                        N3 = (1 / (1 + Phi)) * (3*xi**2 - 2*xi**3 + Phi*xi)
-                        N4 = (L_e / (1 + Phi)) * (-xi**2 + xi**3 - 0.5*Phi*(xi - xi**2))
-                        defl = N1*U_full[2*i] + N2*U_full[2*i+1] + N3*U_full[2*i+2] + N4*U_full[2*i+3]
+                        Le, xi = nodes[i+1] - nodes[i], (x - nodes[i]) / (nodes[i+1] - nodes[i])
+                        Phi = self._get_phi(Le)
+                        N1 = (1/(1+Phi))*(1-3*xi**2+2*xi**3+Phi*(1-xi))
+                        N2 = (Le/(1+Phi))*(xi-2*xi**2+xi**3+0.5*Phi*(xi-xi**2))
+                        N3 = (1/(1+Phi))*(3*xi**2-2*xi**3+Phi*xi)
+                        N4 = (Le/(1+Phi))*(-xi**2+xi**3-0.5*Phi*(xi-xi**2))
+                        d_defl = N1*U[2*i] + N2*U[2*i+1] + N3*U[2*i+2] + N4*U[2*i+3]
                         break
-                res_data.append({'x': x, 'shear': V, 'moment': M, 'deflection': defl})
-
-            return pd.DataFrame(res_data), pd.DataFrame(reac_list), {'load_fy': total_load_fy, 'reac_fy': total_reac_fy, 'load_m0': total_load_m0, 'reac_m0': total_reac_m0}
+                res.append({'x': x, 'shear': v_sh, 'moment': m_bm, 'deflection': d_defl})
+            return pd.DataFrame(res), pd.DataFrame(reac_res), {'l_fy': t_load_fy, 'r_fy': t_reac_fy, 'l_m0': t_load_m0, 'r_m0': t_reac_m0}
         except Exception as e: return pd.DataFrame(), pd.DataFrame(), {"error": str(e)}
