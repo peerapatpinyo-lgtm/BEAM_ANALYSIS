c09168b1bfbaf9192e14ee64a908f566dfc8d8ca
peerapatpinyo-lgtm <peerapat.pinyo@mail.kmutt.ac.th>
2026-01-07 09:46:27 +0700

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,75 +1,76 @@
+
 import numpy as np
 
-class BeamSolver:
-    def __init__(self, spans, supports, loads, b_m, h_m, fc):
-        self.spans = spans  # List [5.0]
-        self.supports = supports  # List [0, 5.0]
-        self.loads = loads  # List of dicts
-        self.b = b_m
-        self.h = h_m
-        self.E = 4700 * np.sqrt(fc) * 1e6  # Pa
-        self.I = (b_m * h_m**3) / 12
-        self.sw_unit = b_m * h_m * 2400 * 9.81  # N/m
+class TimoshenkoBeamSolver:
+    def __init__(self, L, b, h, fc, P_kN, pos_p):
+        self.L = L
+        self.b = b / 1000 # m
+        self.h = h / 1000 # m
+        self.fc = fc
+        self.fy = 400 # MPa
+        self.P = P_kN * 1000 # N
+        self.pos_p = pos_p
+        
+        # คุณสมบัติวัสดุ
+        self.E = 4700 * np.sqrt(fc) * 1e6 # Pa
+        self.G = self.E / (2 * (1 + 0.2)) # Shear Modulus (Poisson's ratio approx 0.2)
+        self.I = (self.b * self.h**3) / 12
+        self.As = self.b * self.h # Area
+        self.kappa = 5/6 # Shear correction factor for rectangular section
+        self.sw_unit = self.As * 2400 * 9.81 # N/m
 
     def solve(self):
-        # 1. Mesh Generation
-        cum_spans = [0] + list(np.cumsum(self.spans))
-        total_l = sum(self.spans)
-        nodes = np.linspace(0, total_l, 51)
-        for ld in self.loads:
-            gx = cum_spans[ld['span_index']] + ld['x']
-            nodes = np.append(nodes, gx)
-        nodes = np.unique(np.sort(nodes))
+        # Mesh: 100 elements เพื่อความละเอียด
+        nodes = np.linspace(0, self.L, 101)
+        if not any(np.isclose(nodes, self.pos_p)):
+            nodes = np.sort(np.append(nodes, self.pos_p))
         
         num_nodes = len(nodes)
         dof = 2 * num_nodes
         K = np.zeros((dof, dof))
         F = np.zeros(dof)
 
-        # 2. Stiffness & Self-weight
         for i in range(num_nodes - 1):
-            L = nodes[i+1] - nodes[i]
+            le = nodes[i+1] - nodes[i]
             idx = [2*i, 2*i+1, 2*(i+1), 2*(i+1)+1]
+            
             EI = self.E * self.I
-            k_local = (EI / L**3) * np.array([
-                [12, 6*L, -12, 6*L], [6*L, 4*L**2, -6*L, 2*L**2],
-                [-12, -6*L, 12, -6*L], [6*L, 2*L**2, -6*L, 4*L**2]
+            GAk = self.G * self.As * self.kappa
+            Phi = (12 * EI) / (GAk * le**2) # Timoshenko's Phi (Shear deformation parameter)
+
+            # Stiffness Matrix for Timoshenko Beam Element
+            const = EI / (le**3 * (1 + Phi))
+            k_local = const * np.array([
+                [12, 6*le, -12, 6*le],
+                [6*le, (4+Phi)*le**2, -6*le, (2-Phi)*le**2],
+                [-12, -6*le, 12, -6*le],
+                [6*le, (2-Phi)*le**2, -6*le, (4+Phi)*le**2]
             ])
             K[np.ix_(idx, idx)] += k_local
-            # Force from Self-weight
-            F[2*i] -= self.sw_unit * L / 2
-            F[2*i+1] -= self.sw_unit * L**2 / 12
-            F[2*(i+1)] -= self.sw_unit * L / 2
-            F[2*(i+1)+1] += self.sw_unit * L**2 / 12
 
-        # 3. Add External Point Loads
-        for ld in self.loads:
-            gx = cum_spans[ld['span_index']] + ld['x']
-            nid = np.argmin(np.abs(nodes - gx))
-            F[2*nid] -= ld['mag'] * 1000
+            # Force Vector (Self-weight)
+            F[2*i] -= self.sw_unit * le / 2
+            F[2*(i+1)] -= self.sw_unit * le / 2
 
-        # 4. Boundary Conditions
+        # Point Load
+        p_node = np.argmin(np.abs(nodes - self.pos_p))
+        F[2*p_node] -= self.P
+
+        # Boundary Conditions
         active_dof = np.ones(dof, dtype=bool)
-        for s_pos in self.supports:
-            nid = np.argmin(np.abs(nodes - s_pos))
-            active_dof[2*nid] = False 
-
+        active_dof[0] = False # Vertical at 0
+        active_dof[2*(num_nodes-1)] = False # Vertical at L
+        
         u = np.zeros(dof)
         u[active_dof] = np.linalg.solve(K[np.ix_(active_dof, active_dof)], F[active_dof])
 
-        # 5. Get Internal Forces (Max Moment & Shear)
+        # คำนวณ Moment และ Shear
         moments = []
-        shears = []
         for i in range(num_nodes - 1):
-            L = nodes[i+1] - nodes[i]
-            idx = [2*i, 2*i+1, 2*(i+1), 2*(i+1)+1]
-            u_el = u[idx]
-            # Moment calculation from nodal displacements
-            m_start = (EI / L**2) * np.array([-6, -4*L, 6, -2*L]) @ u_el
-            m_end = (EI / L**2) * np.array([6, 2*L, -6, 4*L]) @ u_el
-            # Shear force calculation
-            v_el = (EI / L**3) * np.array([12, 6*L, -12, 6*L]) @ u_el
-            moments.extend([abs(m_start), abs(m_end)])
-            shears.append(abs(v_el))
+            le = nodes[i+1] - nodes[i]
+            u_el = u[[2*i, 2*i+1, 2*(i+1), 2*(i+1)+1]]
+            # Moment in Timoshenko element
+            m_start = (EI / (le * (1 + Phi))) * (-(u_el[1]*(4+Phi) + u_el[3]*(2-Phi)) + (6/le)*(u_el[2]-u_el[0]))
+            moments.append(abs(m_start))
 
-        return max(moments)/1000, max(shears)/1000 # Return kNm, kN
+        return max(moments) / 1000 # kNm
