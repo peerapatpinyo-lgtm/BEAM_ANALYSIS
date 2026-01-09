0ff7981721c2f2c7044cb6b828f7b448fe7c6058
peerapatpinyo-lgtm <peerapat.pinyo@mail.kmutt.ac.th>
2026-01-09 15:01:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-app.py
+#app.py
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -173,3 +173,4 @@ else:
 
     except Exception as e:
         st.error(f"❌ **System Error:** {e}")
+
